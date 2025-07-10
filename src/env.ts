694a5e9fc554892a0f71745406f5175668fb8a48
Lucas Heber <lucas.heber07@gmail.com>
2025-07-10 17:51:50 -0300

    feat(db): establish database connection and update environment configuration

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,7 @@ import { z } from 'zod';
 
 const envSchema = z.object({
     PORT: z.coerce.number().default(3000),
+    DATABASE_URL: z.string().startsWith('postgresql://'),
 });
 
-const env = envSchema.parse(process.env);
-
-export { env };
+export const env = envSchema.parse(process.env);
