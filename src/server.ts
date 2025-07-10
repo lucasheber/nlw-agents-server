2410a9c3c500aee55ba23617029138496484e89b
Lucas Heber <lucas.heber07@gmail.com>
2025-07-10 17:21:57 -0300

    feat(env): add environment configuration and server setup

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+    origin: '*', // Allow all origins for development purposes
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.get('/health', () => {
+    return { message: 'OK' };
+});
+
+app.listen({ port: env.PORT });
