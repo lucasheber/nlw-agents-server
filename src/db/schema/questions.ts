b804b1c4e8b4a6c9e81ee9f2a8f1790442acf022
Lucas Heber <lucas.heber07@gmail.com>
2025-07-11 17:16:00 -0300

    feat(schema): add questions schema and update export structure

---
+++ src/db/schema/questions.ts
@@ -0,0 +1,15 @@
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { rooms } from './rooms.ts';
+
+export const questions = pgTable('questions', {
+    id: uuid().primaryKey().defaultRandom(),
+    roomId: uuid()
+        .notNull()
+        .references(() => rooms.id),
+    question: text().notNull(),
+    createdAt: timestamp().notNull().defaultNow(),
+    updatedAt: timestamp()
+        .$onUpdateFn(() => new Date())
+        .notNull()
+        .defaultNow(),
+});
